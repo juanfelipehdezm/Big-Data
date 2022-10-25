436d23915a18bbaf9c1f3afeb0900247f34848b4
Felipe Hernandez <72527112+juanfelipehdezm@users.noreply.github.com>
2022-10-25 15:29:44 -0500

    final dag

--- Airflow/dags/user_processing.py
+++ Airflow/dags/user_processing.py
@@ -8,6 +8,7 @@ from airflow.providers.http.operators.http import SimpleHttpOperator
 import json
 import pandas as pd
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 # function to transform the json we receive from the API
 
@@ -17,15 +18,26 @@ def process_user(ti):
     user = ti.xcom_pull(task_ids="extract_user")
     user = user["results"][0]
     processed_user = pd.json_normalize(
-        {"firstname": user["name"]["first"],
-         "lastname": user["name"]["last"],
-         "country": user["location"]["country"],
-         "username": user["login"]["username"],
-         "password": user["login"]["password"],
-         "email": user["email"]
-         }
+        {
+            "firstname": user["name"]["first"],
+            "lastname": user["name"]["last"],
+            "country": user["location"]["country"],
+            "username": user["login"]["username"],
+            "password": user["login"]["password"],
+            "email": user["email"]
+        }
     )
     processed_user.to_csv("/tmp/processed_user.csv", index=None, header=False)
+
+# function to send the csv to the postgreSQl
+
+
+def store_user():
+    hook = PostgresHook(postgres_conn_id="postgres")
+    hook.copy_expert(
+        sql="COPY users FROM stdin WITH DELIMITER AS ','",
+        filename="/tmp/processed_user.csv"
+    )
 
 
 # Creating "user_processing" dag, the catchup is set to false so it wont runn old DAGs
@@ -37,11 +49,14 @@ with DAG(dag_id="user_processing", start_date=datetime(2022, 10, 24),
         task_id="create_table",
         postgres_conn_id="postgres",
         sql="""
-                CREATE TABLE IF NOT EXISTS users (
+                DROP TABLE IF EXISTS users;
+
+                CREATE TABLE users (
                     firstname TEXT NOT NULL,
                     lastname TEXT NOT NULL,
                     country TEXT NOT NULL,
                     username TEXT NOT NULL,
+                    password TEXT NOT NULL,
                     email TEXT NOT NULL
 
                 );
@@ -66,9 +81,19 @@ with DAG(dag_id="user_processing", start_date=datetime(2022, 10, 24),
         log_response=True
     )
 
-    # Python operator to execute a python function
+    # Python operator to execute a python function ///OPERATOR = ACTION
     process_user = PythonOperator(
         task_id="process_user",
         python_callable=process_user
 
     )
+
+    # python operator to transfer the data from csv to SQL ///OPERATION = ACTION
+    store_user = PythonOperator(
+        task_id="store_user",
+        python_callable=store_user
+    )
+
+    # CREATING THE DEPENDANCIES
+
+    create_table >> is_api_available >> extract_user >> process_user >> store_user
